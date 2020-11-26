98ca73302acfcbcb3efca5c2d73d8ff6c0a4128a
김동균 <nabilera1@naver.com>
2020-11-26 15:34:36 +0900

    초기 커밋.

---
+++ 코딩테스트_리스트_총합/코딩테스트_리스트_총합.py
@@ -0,0 +1,15 @@
+#1 2 3 4
+#총합은 10.
+
+numlist=input()
+n_list=map(int,numlist.split(" "))
+s=sum(n_list)
+#print(f"총합은 {s}.")
+#print("총합은 "+str(s))
+print("총합은 {}".format(s))
+
+
+
+#파이썬 포맷
+#https://pyformat.info/
+
